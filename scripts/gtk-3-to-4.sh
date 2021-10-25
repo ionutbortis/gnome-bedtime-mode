a83c579ca681725cb29469270c71a85ef6929677
ionutbortis <ionutbortis@yahoo.com>
2021-10-25 15:15:10 +0300

    Script for aiding with the gtk 3 to 4 conversion

---
+++ scripts/gtk-3-to-4.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SCRIPTS_FOLDER="$( dirname "$(realpath -s "$0")" )"
+
+source "$SCRIPTS_FOLDER/_vars.sh" "$@"
+
+GTK3_UI_FILE="$PROJECT_ROOT/src/ui/gtk3/preferences.ui"
+GTK4_UI_FILE="$PROJECT_ROOT/src/ui/gtk4/preferences.ui"
+
+convert_to_gtk4_format() {
+  echo "Calling gtk4-builder-tool for 3 to 4 conversion..."
+
+  gtk4-builder-tool simplify --3to4 "$GTK3_UI_FILE" > "$GTK4_UI_FILE"
+}
+
+convert_to_max_width_chars() {
+  echo "Converting 'max-length' properties to 'max-width-chars'..."
+
+  sed -i 's/<property name="max-length">/<property name="max-width-chars">/g' "$GTK4_UI_FILE"
+}
+
+remove_can_focus_properties() {
+  echo "Removing 'can-focus' properties..."
+
+  sed -i '/<property name="can-focus">0<\/property>/d' "$GTK4_UI_FILE"
+}
+
+remove_empty_lines() {
+  echo "Removing empty lines..."
+
+  sed -i '/^\s*$/d' "$GTK4_UI_FILE"
+}
+
+convert_to_gtk4_format
+convert_to_max_width_chars
+remove_can_focus_properties
+remove_empty_lines
