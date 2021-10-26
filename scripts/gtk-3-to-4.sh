b6aa7ed41c788932d58d7803be591b3218a3a461
ionutbortis <ionutbortis@yahoo.com>
2021-10-26 10:28:33 +0300

    Added script check for gtk4 tools

--- scripts/gtk-3-to-4.sh
+++ scripts/gtk-3-to-4.sh
@@ -7,6 +7,14 @@ source "$SCRIPTS_FOLDER/_vars.sh" "$@"
 GTK3_UI_FILE="$PROJECT_ROOT/src/ui/gtk3/preferences.ui"
 GTK4_UI_FILE="$PROJECT_ROOT/src/ui/gtk4/preferences.ui"
 
+check_gtk4_tools_availability() {
+  if ! command -v gtk4-builder-tool &> /dev/null; then
+    echo "ERROR: Could not find the 'gtk4-builder-tool' command!"
+    echo "Please ensure the 'gtk4-devel' package is installed."
+    exit 1
+  fi
+}
+
 convert_to_gtk4_format() {
   echo "Calling gtk4-builder-tool for 3 to 4 conversion..."
 
@@ -31,6 +39,7 @@ remove_empty_lines() {
   sed -i '/^\s*$/d' "$GTK4_UI_FILE"
 }
 
+check_gtk4_tools_availability
 convert_to_gtk4_format
 convert_to_max_width_chars
 remove_can_focus_properties
