d6c667933d9b7bd969a828422ff050543a20ff98
ionutbortis <ionutbortis@yahoo.com>
2022-04-08 15:40:48 +0300

    Workaround for the nested scroll issue on Gnome 42 This is a temporary workaround in order to fix the Libadwaita nested GtkScrolledWindow issue on Gnome 42. The plan is to redesign the UI according to Libadwaita principles and support new features only for Gnome 42 and onward.

--- src/prefs.js
+++ src/prefs.js
@@ -13,3 +13,16 @@ function buildPrefsWidget() {
   const preferences = new Preferences();
   return preferences.widget;
 }
+
+// This is a temporary workaround in order to fix the Libadwaita
+// nested GtkScrolledWindow issue on Gnome 42.
+// The plan is to redesign the UI according to Libadwaita principles
+// and support new features only for Gnome 42 and onward.
+function fillPreferencesWindow(window) {
+  const box = new imports.gi.Gtk.Box({
+    orientation: imports.gi.Gtk.Orientation.VERTICAL,
+  });
+  box.append(new imports.gi.Adw.HeaderBar());
+  box.append(buildPrefsWidget());
+  window.set_content(box);
+}
